7ef3f2c9e81324f15fd542f444f217abaa9b43fc
Dan <dan.sasaki12@gmail.com>
2018-06-25 20:03:00 -0600

    update

--- src/Components/Jumbotron/Jumbotron.js
+++ src/Components/Jumbotron/Jumbotron.js
@@ -3,7 +3,7 @@ import "./Jumbotron.css";
 
 // Jumbotron component
 const Jumbotron = props => (
-  <div className="jumbotron jumbotron-fluid bg-cover text-white text-center">
+  <div className="jumbotron jumbotron-fluid text-white text-center">
     <h1 className="display-4 font-weight-bold">Clicky Game!</h1>
     <p className="lead font-weight-bold">Click on an image to earn points, but don't click the same image twice!</p>
   </div>
